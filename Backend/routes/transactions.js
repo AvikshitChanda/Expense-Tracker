2db8ba7e17200d1de1217877ffe2f7c2391ff149
AvikshitChanda <107202881+AvikshitChanda@users.noreply.github.com>
2024-07-03 12:01:19 +0530

    Update transactions.js

--- Backend/routes/transactions.js
+++ Backend/routes/transactions.js
@@ -5,17 +5,17 @@ const authMiddleware = require('../middlewares/auth');
 
 const router = express.Router();
 
-// Income routes
+
 router.post('/add-income', authMiddleware, addIncome);
 router.get('/get-incomes', authMiddleware, getIncomes);
 router.delete('/delete-income/:id', authMiddleware, deleteIncome);
 
-// Expense routes
+
 router.post('/add-expense', authMiddleware, addExpense);
 router.get('/get-expenses', authMiddleware, getExpenses);
 router.delete('/delete-expense/:id', authMiddleware, deleteExpense);
 
-// New route for fetching total stats
+
 router.get('/total-expense', authMiddleware,getTotalExpense );
 router.get('/total-income', authMiddleware,getTotalIncome );
 
