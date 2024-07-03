88159e05e327dce8604ef31f2b11e98dab64f4cb
AvikshitChanda <107202881+AvikshitChanda@users.noreply.github.com>
2024-07-03 11:05:43 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://expense-tracker-657a.onrender.com'
+  baseURL: 'https://expense-tracker-657a.onrender.com/api'
 });
 
 export default api;
