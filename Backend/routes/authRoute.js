70ffb6f5da940ecdd98dad2426c7ae62d84c4c04
AvikshitChanda <107202881+AvikshitChanda@users.noreply.github.com>
2024-07-03 12:00:58 +0530

    Update authRoute.js

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -3,12 +3,9 @@ const router = express.Router();
 const { register, login,getUserProfile } = require('../controllers/authController');
 const authMiddleware=require('../middlewares/auth')
 
-// Route: POST /api/auth/register
-// Description: Register a new user
+
 router.post('/register', register);
 
-// Route: POST /api/auth/login
-// Description: Login user
 router.post('/login', login);
 
 router.get('/profile', authMiddleware, getUserProfile);
