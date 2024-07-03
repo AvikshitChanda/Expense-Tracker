6d2b6d3e692a607ed3e5c744317f211e0be89372
AvikshitChanda <107202881+AvikshitChanda@users.noreply.github.com>
2024-07-03 12:00:18 +0530

    Update auth.js

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -1,19 +1,19 @@
 const jwt = require('jsonwebtoken');
-const jwtSecret = process.env.JWT_SECRET; // Ensure jwtSecret is correctly imported
+const jwtSecret = process.env.JWT_SECRET;
 
 const authMiddleware = (req, res, next) => {
-  // Check if there is a token in the headers
+  
   const token = req.header('Authorization')?.replace('Bearer ', '');
-   // Log the token here
+
 
   if (!token) {
     return res.status(401).json({ message: 'Authorization denied. No token provided.' });
   }
 
   try {
-    // Verify token
+   
     const decoded = jwt.verify(token, jwtSecret);
-    req.user = decoded.user; // Assuming your JWT payload has a 'user' field
+    req.user = decoded.user; 
     next();
   } catch (error) {
     console.error('Token verification failed:', error.message);
