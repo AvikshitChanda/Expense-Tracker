ca50c212e77fd217dd699708a122a1f81f9fdc0c
AvikshitChanda <107202881+AvikshitChanda@users.noreply.github.com>
2024-07-03 11:58:11 +0530

    Update authController.js

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,23 +1,23 @@
 const bcrypt = require('bcryptjs');
 
-const jwt = require('jsonwebtoken'); // For generating JSON web tokens
+const jwt = require('jsonwebtoken'); 
 const User = require('../models/user');
 
-// Register a new user
+
 const register = async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    // Check if user already exists
+    
     let user = await User.findOne({ username });
     if (user) {
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    // Hash the password
+   
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create a new user
+   
     user = new User({
       username,
       password: hashedPassword,
@@ -31,24 +31,24 @@ const register = async (req, res) => {
   }
 };
 
-// Login user
+
 const login = async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    // Check if user exists
+   
     const user = await User.findOne({ username });
     if (!user) {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
 
-    // Validate password
+   
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
 
-    // Generate and return a JSON web token (JWT)
+    
     const payload = {
       user: {
         id: user.id,
@@ -57,7 +57,7 @@ const login = async (req, res) => {
     };
     console.log('Username in payload:', payload.user.username);
 
-    // Sign the token
+  
     jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '108h' }, (err, token) => {
       if (err) throw err;
       console.log('Generated token:', token);
@@ -68,7 +68,7 @@ const login = async (req, res) => {
     res.status(500).send('Server Error');
   }
 };
-// Fetch user profile
+
 const getUserProfile = async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
